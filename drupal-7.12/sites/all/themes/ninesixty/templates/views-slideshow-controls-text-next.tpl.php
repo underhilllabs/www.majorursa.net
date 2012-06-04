3b8480cc076f385a14b91db94eb6577d95c0a589
Gray Jay <grayjay@denvertech.org>
2012-06-04 15:23:58 -0400

    Added front page slideshow controls view templates

--- drupal-7.12/sites/all/themes/ninesixty/templates/views-slideshow-controls-text-next.tpl.php
+++ drupal-7.12/sites/all/themes/ninesixty/templates/views-slideshow-controls-text-next.tpl.php
@@ -1,1 +1,1 @@
-<span id="views_slideshow_controls_text_next_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>"><a href="#"><img src="../images/arrow_right.png" width=20></a></span>
+<span id="views_slideshow_controls_text_next_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>"><a href="#"><img src="/sites/all/themes/ninesixty/images/arrow_right.png" width=20></a></span>
--- drupal-7.12/sites/all/themes/ninesixty/templates/views-slideshow-controls-text-previous.tpl.php
+++ drupal-7.12/sites/all/themes/ninesixty/templates/views-slideshow-controls-text-previous.tpl.php
@@ -1,1 +1,1 @@
-<span id="views_slideshow_controls_text_previous_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>"><a href="#"><img src="../images/arrow_left.png" width=20></a></span>
+<span id="views_slideshow_controls_text_previous_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>"><a href="#"><img src="/sites/all/themes/ninesixty/images/arrow_left.png" width=20></a></span>
